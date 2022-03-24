ed143818bb448855f45ab7d48d822646b9f739cc
001160091 <Anjali.SHESHAPPA@student.tafesa.edu.au>
2022-03-24 13:25:34 +1030

    modified second time

--- Student Files/Ticketing/Ticketing/TicketPrice.cs
+++ Student Files/Ticketing/Ticketing/TicketPrice.cs
@@ -13,10 +13,10 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 35.5m;
-        const decimal mdecGeneral = 25.75m;
-        const decimal mdecBox = 62.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecBalcony = 38.5m;
+        const decimal mdecGeneral = 28.75m;
+        const decimal mdecBox = 64.0m;
+        const decimal mdecDiscount = 6.0m; 
 
         private int Section
         {
